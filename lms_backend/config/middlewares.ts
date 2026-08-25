a8cd045078940163b09b063a1e1990519832bc7a
Shafiul Islam Mashruf <bemashrufislam@gmail.com>
2026-08-25 14:49:55 +0600

    fix: whitelist vercel frontend domain in strapi cors config

--- lms_backend/config/middlewares.ts
+++ lms_backend/config/middlewares.ts
@@ -4,7 +4,16 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://learningmanagementsystem-self.vercel.app',
+      ],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
